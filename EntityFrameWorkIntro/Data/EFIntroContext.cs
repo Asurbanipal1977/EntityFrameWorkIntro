19372a94421ffc4973b01f73a64a8294857a01fe
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-10-28 15:06:32 +0200

    Agregar archivos de proyecto.

---
+++ EntityFrameWorkIntro/Data/EFIntroContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameWorkIntro.Models;
+
+namespace EntityFrameWorkIntro.Data
+{
+    public class EFIntroContext : DbContext
+    {
+        public EFIntroContext (DbContextOptions<EFIntroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityFrameWorkIntro.Models.Pais> Paises { get; set; }
+
+        public DbSet<EntityFrameWorkIntro.Models.Alumno> Alumnos { get; set; }
+    }
+}
---
+++ EntityFrameWorkIntro/Models/Alumno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkIntro.Models
+{
+	public class Alumno
+	{
+		public long Id { get; set; }
+		public string Nombre { get; set; }
+		public string Apellidos { get; set; }
+		public string Dni { get; set; }
+
+		[Display(Name = "Fecha de Nacimiento")]
+		public string FechaNacimiento { get; set; }
+		public long Pais { get; set; }
+
+		[Display(Name = "Estado Civil")]
+		public string EstadoCivil { get; set; }
+	}
+}
